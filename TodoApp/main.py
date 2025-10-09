c3790c7b78cffb5dac3d320c99aa851005ca4775
LAzazel <mironrebrov@gmail.com>
2025-10-09 23:10:29 +0300

    first commit

---
+++ TodoApp/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from database import engine
+from models import Base
+from routers import auth, todos
+
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
